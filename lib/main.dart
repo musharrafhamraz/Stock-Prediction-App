679136431883489d18a25aed1814a2a27c679fcd
musharrafhamraz <musharrafhamraz02@gmail.com>
2025-02-14 14:13:28 +0500

    entry point redirected

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
-import 'chat_screen.dart';
+import 'screens/splash_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: ChatScreen(),
-  ));
+  runApp(const ChatBotApp());
+}
+
+class ChatBotApp extends StatelessWidget {
+  const ChatBotApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'School ChatBot',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
